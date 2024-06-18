fa3a470118bdda714385502bd26008f901b4bf8e
aldsouza <altondsouza02@gmail.com>
2024-06-17 21:46:49 -0600

    Client Example

--- llm_client/llm_client.py
+++ llm_client/llm_client.py
@@ -1,6 +1,6 @@
 import requests
 
-inputs = {"input": {"question": "Tell me 3 ways given to prevent heart disease", "chat_history": []}}
+inputs = {"input": {"question": "how does maslows triangle help?", "chat_history": []}}
 response = requests.post("http://localhost:8000/invoke", json=inputs)
 
 print(response.text)
