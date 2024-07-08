3267d6710c24323e10fdea13f1d2f2e8825052d4
aldsouza <altondsouza02@gmail.com>
2024-07-08 15:11:49 -0600

    Client Example

--- llm_client/llm_client.py
+++ llm_client/llm_client.py
@@ -1,6 +1,9 @@
 import requests
 
-inputs = {"input": {"question": "how does maslows triangle help?", "chat_history": []}}
-response = requests.post("http://localhost:8000/invoke", json=inputs)
-
-print(response.text)
+inputs = {"input": {"question": "What is Heart Disease", "chat_history": []}}
+inputs1 = {"input": {"question": "For Patty Johnson get me the last 5 encounters."}}
+# response = requests.post("http://localhost:8000/general_info/invoke", json=inputs)
+response1 = requests.post("http://localhost:8000/summarize_user_reports/invoke", json=inputs1)
+# response = requests.post("http://104.197.203.11:8000/invoke", json=inputs)
+# print(response.text)
+print(response1.text)
